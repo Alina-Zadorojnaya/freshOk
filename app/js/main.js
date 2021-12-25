d37eafc36ec75586209c35dd94666fd252df95ae
Alina-Zadorojnaya <alinasmartmebel@gmail.com>
2021-12-25 22:00:52 +0200

    catalog

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,14 @@
 $(function () {
 
 
-
     $('.main-slider__list').slick({
         infinite: true,
         slidesToShow: 1,
         slidesToScroll: 1
     });
 
-
+   
 
 });
+
+var mixer = mixitup('.catalog__inner');
--- gulpfile.js
+++ gulpfile.js
@@ -41,7 +41,8 @@ function images() {
 function scripts() {
     return src([
         'node_modules/jquery/dist/jquery.js',
-        'node_modules/slick-carousel/slick/slick.js'
+        'node_modules/slick-carousel/slick/slick.js',
+        'node_modules/mixitup/dist/mixitup.js'
     ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
