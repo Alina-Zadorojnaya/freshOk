76ee8bffec9fd6b8375b9d3ac2bcbea2093aaeae
Alina-Zadorojnaya <alinasmartmebel@gmail.com>
2022-01-09 18:16:34 +0200

    fiad: changes made to lesson 12

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,20 @@
+//const autoPrefixer = require("gulp-autoprefixer");
+
 $(function () {
 
 
-    $('.main-slider__list').slick({
+ 
+   $('.main-slider__list').slick({
         infinite: true,
         slidesToShow: 1,
-        slidesToScroll: 1
-    });
+        slidesToScroll: 1,
+        adaptiveHeight: true
+      });
 
-   
+
+   $('.nav__catalog').on('click', function(){
+    $(this).toggleClass('nav__catalog--active')
+   });
 
 });
 
