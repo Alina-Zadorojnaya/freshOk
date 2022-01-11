a2a894d92f0da4e1f83f6e041cd09f9a8c4588e3
Alina-Zadorojnaya <alinasmartmebel@gmail.com>
2022-01-12 00:48:39 +0200

    home w-12

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,6 @@
 //const autoPrefixer = require("gulp-autoprefixer");
 
 $(function () {
-
-
  
    $('.main-slider__list').slick({
         infinite: true,
@@ -11,11 +9,42 @@ $(function () {
         adaptiveHeight: true
       });
 
+      $('.slider-brends__inner').slick({
+        infinite: true,
+        slidesToShow: 6,
+        slidesToScroll: 2,
+        adaptiveHeight: true,
+        arrows: false
+      });
 
    $('.nav__catalog').on('click', function(){
     $(this).toggleClass('nav__catalog--active')
    });
 
+   $('.catalog__inne').mixItUp({
+    selectors: {
+      filter: '.filter'
+    }
+  });
+
+  $('.promo-card__inner').mixItUp({
+    selectors: {
+      filter: '.filter2'
+    }
+  });
+  
+
 });
 
-var mixer = mixitup('.catalog__inner');
+
+var containerCard = document.querySelector('[data-ref="navigation-mix__container-card"]');
+var containerPromo = document.querySelector('[data-ref="navigation-mix__container-promo"]');
+ 
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+ 
+  var mixer1 = mixitup(containerCard, config);
+  var mixer2 = mixitup(containerPromo, config);
